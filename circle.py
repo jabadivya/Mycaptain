de113af6a23a0b3a64242c3efc4ee2b7991745ee
jabadivya <109748271+jabadivya@users.noreply.github.com>
2022-07-21 21:10:45 +0530

    circumference of circle
    to find circumference of a circle using python

---
+++ circle.py
@@ -0,0 +1,8 @@
+from math import pi
+r = float ("input the radius of the circle :"))
+Print ( "The area of circle with radius " + str (r) + is : "+ str (Pi*r**2)
+
+
+output
+Input the radius of the circle :1.1
+The area of the circle with radius 1.1 is : 3.80132
