9c24c961264d5da2e6ca567f7e2428af7ca52c2e
jabadivya <109748271+jabadivya@users.noreply.github.com>
2022-07-21 21:33:03 +0530

    circuference of circle and extenstion of filr
    1.To find the radius of the circle where str =string
    2. the extension of the file where repr is a developer friendly that returns a printable representation of the given object

--- assignment 1.py
+++ assignment 1.py
@@ -6,3 +6,12 @@ Print ( "The area of circle with radius " + str (r) + is : "+ str (Pi*r**2)
 output
 Input the radius of the circle :1.1
 The area of the circle with radius 1.1 is : 3.80132
+
+   
+filename = input (" Input the  filename :  ")
+  f_extns = filename .split (".")
+  print ( " The extension of the file is : "n + repr(f_extns[-1]))
+       
+ output
+ Input the filname : abcd.py
+ The extension of the file is : "python"
