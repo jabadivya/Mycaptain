e243cc2a4d2740d965bf07cdbd9e4c7b0ace4c9e
jabadivya <109748271+jabadivya@users.noreply.github.com>
2022-07-22 20:35:43 +0530

    To print all positive number in a range

---
+++ assignment 3.py
@@ -0,0 +1,34 @@
+# list of numbers
+list1 =[12,-7,5,65,-14]
+num = 0
+  
+# using while loop 
+while(num < len(list)) :
+  
+  
+    # checking condition
+    if list[num] >=0:
+        print(list1[num] , end =" ")
+  
+    # increment num
+    num += 1
+  
+  output
+ [12,5,64]
+  
+ # list of numbers
+ list2 =[12,14,-95,3]
+ num = 0
+   
+ # using while loop
+ while(num < len(list)) :
+   
+     # checking condition
+     if list[num] >=0:
+        print(list1[num] , end =" ")
+   
+     #increment num
+     num += 1
+  
+  output
+ [12,14,3]
