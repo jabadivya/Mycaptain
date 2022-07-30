ebe5e651c56e21bbef5ff1d14b1cff4a664d9c54
jabadivya <109748271+jabadivya@users.noreply.github.com>
2022-07-30 22:47:37 +0530

    circle and file extension
    cicumference of circle and extention of file

---
+++ assignment 5.py
@@ -0,0 +1,13 @@
+from math import pi
+r = float ("input the circumference of circle:")
+print (" The circumference of circle "+ str () + is : " + str (pi*r**2)")
+
+output 
+input the circumference of circle 1.1
+The circumference of circle 1.1 is :3.80132
+  
+  filename = input ("Input the filename :")
+  f_extns = filename .split (".")
+  print (" The extension of the file is :")
+  
+ 
