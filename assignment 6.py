c3cd61fa276c5066f467eaa8e06411c88ae87a66
jabadivya <109748271+jabadivya@users.noreply.github.com>
2022-08-01 00:24:57 +0530

    school admission

---
+++ assignment 6.py
@@ -0,0 +1,40 @@
+import csv
+def_write_into_csv(list):
+  writeopen("student_info.csv","+",'a',new line ") as csv_file:
+            writer = csv.written(csv_file)
+            if csv_file.tell == 0:
+            write.writenrow("name","age","contact number","E mail id")
+if_name_ ==" _void_":
+            condition = true
+            student_num=1
+        while (condition):
+            student_info=input ("enter sum of student information sor student 1() in the following format ("name age contact_number E mail id:". format(student_num))
+            
+            #split
+            student_info_list= student_info.split(' ')
+            print("enter splitup information is:_str(student_info_list))
+            print ("The entered information is =/n name :()/n age :()/n contact number :()/n Email id :() "
+            .format(student_info_list(0), student_info_list(1), student_info_list(2),student_info_list(3))
+            choice_check=input("is the entered information correct (yes/no) :")
+        if choice_check is == 'yes'
+            wite_info=csv(student_info_list)
+            condition_check = input("enter(yes/no) if you want to enter information of another student")
+        if condition_check=='yes'
+                   condition is true
+            student_num=student_num=1
+            else condition_check==no
+                   condition=false
+             else if choice_check ='no'
+             print ("/n please re enter the value ")
+                   
+                   
+ output 
+                   
+ enter the student information
+ name = ragu
+ age = 19
+ contact number = 9000332345
+ E mail id = abcd@gmail.com
+                                
+                              
+            
